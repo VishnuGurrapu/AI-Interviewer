bbb056a1c1677112b2cc6826a0ab4d3f3acd2888
Vishnu Gurrapu <gurrapuvishnu20@gmail.com>
2025-09-30 22:37:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ process.on('unhandledRejection', (reason, promise) => {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:5173'], // Allow all dev server ports
+  origin: ['http://localhost:8080', 'http://localhost:8081','https://ai-interviewer-vish.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
